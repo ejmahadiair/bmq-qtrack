f9755ebd938afbb25bce9295fdd0d1d01c3d2c8f
bdbeponi <devs@skillerszone.com>
2025-05-11 13:38:12 +0200

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/dashboard");
+  }, [router]);
+
   return (
     <>
-     <div>
-
-     </div>
+      <div>Loading...</div>
     </>
   );
 }
